c162a743a250b4fde485eb1324131ef7954572e0
jacobmelendez17 <nerdalert46@gmail.com>
2026-03-14 15:19:00 -0700

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+
+export default function Home() {
+  return (
+    <Button size="xs" onClick={() => toast.success("hello world")}>
+      Click Me
+    </Button>
+  );
+}
