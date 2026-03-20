4ae8f0645218d94f51dde1f3107271ec45610eed
jacobmelendez17 <nerdalert46@gmail.com>
2026-03-19 17:37:36 -0700

    Added prisma database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-"use client"
-
-import { Button } from "@/components/ui/button";
-import { toast } from "sonner";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <Button size="xs" onClick={() => toast.success("hello world")}>
-      Click Me
-    </Button>
-  );
+    <div className="flex min-h-screen fex-col items-center justify-center gap-4 bg-background">
+      <h1 className="text-2xl font-semibold">Welcome to Reverb</h1>
+      <div className="flex items-center gap-4">
+        <OrganizationSwitcher />
+        <UserButton />
+      </div>
+    </div>
+  )
 }
